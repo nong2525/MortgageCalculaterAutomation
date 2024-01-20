f8445685aff9725d21c5875fbb463c513d03b312
nongj <nongj@hellmaster>
2024-01-20 12:50:11 -0500

    updating new code on addition class

---
+++ MortgageCalculatorGitPractice/src/main/java/com/application/Addition.java
@@ -0,0 +1,10 @@
+package com.application;
+
+public class Addition {
+	public int add(int x, int y) {
+		int result = x +y;
+		System.out.println("x + y result = " +result);
+		return result;
+	}
+
+}
--- MortgageCalculatorGitPractice/src/main/java/com/application/MortgageCalculatorApp.java
+++ MortgageCalculatorGitPractice/src/main/java/com/application/MortgageCalculatorApp.java
@@ -5,6 +5,13 @@ package com.application;
 public class MortgageCalculatorApp {
 	public static void main(String[] args) {
 		System.out.println("Mortgage Calculator application is starting ");
+	Addition add = new Addition();
+	int result = add.add(5, 7);
+	System.out.println("x + y = "+result);
+	
+	
 	}
+	
+	
 
 }
