4e27e48d98801e8a5fe412e16d147d5536ed17b7
nongj <nongj@hellmaster>
2024-01-20 11:29:00 -0500

    adding main method and print statement

--- MortgageCalculatorGitPractice/src/main/java/com/application/MortgageCalculatorApp.java
+++ MortgageCalculatorGitPractice/src/main/java/com/application/MortgageCalculatorApp.java
@@ -3,5 +3,8 @@ package com.application;
  * @author nongj
  */
 public class MortgageCalculatorApp {
+	public static void main(String[] args) {
+		System.out.println("Mortgage Calculator application is ");
+	}
 
 }
